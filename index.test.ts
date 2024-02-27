7f5ab54d8abfcdfe67a762ca65757d80512250fe
Nathan Sainsbury <nasainsbury@gmail.com>
2024-02-27 13:33:32 +0000

    adding more tests

--- index.test.ts
+++ index.test.ts
@@ -31,3 +31,41 @@ describe("Setting strings", () => {
     expect(res).toEqual(139.4);
   });
 });
+
+describe("Setting arrys", () => {
+  const redis = new Redis();
+
+  it("Successfully creates an array of strings", async () => {
+    await redis.set("Names", ["Nathan", "James", "John", "Willow"]);
+    const res = await redis.get("Names");
+    expect(res).toEqual(["Nathan", "James", "John", "Willow"]);
+  });
+
+  it("Successfully overwrites an array of strings", async () => {
+    await redis.set("Names", ["Nathan", "James", "John", "Willow"]);
+    const res = await redis.get("Names");
+    expect(res).toEqual(["Nathan", "James", "John", "Willow"]);
+
+    await redis.set("Names", ["Johnny", "Jimmy"]);
+    const res2 = await redis.get("Names");
+    expect(res2).toEqual(["Johnny", "Jimmy"]);
+  });
+
+  it("Successfully creates an array of ints", async () => {
+    await redis.set("Ages", [25, 33, 49, 10004]);
+    const res = await redis.get("Ages");
+    expect(res).toEqual([25, 33, 49, 10004]);
+  });
+
+  it("Successfully creates an array of floats", async () => {
+    await redis.set("Stats", [20.01, 30.002, 14.7]);
+    const res = await redis.get("Stats");
+    expect(res).toEqual([20.01, 30.002, 14.7]);
+  });
+
+  it("Successfully creates an array of mixed values", async () => {
+    await redis.set("me", ["Nathan", 27, 183.5]);
+    const res = await redis.get("me");
+    expect(res).toEqual(["Nathan", 27, 183.5]);
+  });
+});
